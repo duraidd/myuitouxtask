f8ad52977a4b63ed25a5a3572af272541137de4a
duraidd <duraiessakimuthu@gmail.com>
2024-07-26 11:12:54 +0530

    update

---
+++ backend/models/products/product_model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    productName: {
+        type: String
+      },
+      sku: {
+        type: String
+      },
+      sellingPrice: {
+        type: Number
+      },
+      orginalPrice: {
+        type: Number
+      },
+      type: {
+          type: Number
+      },
+      ratings: {
+          type: Number
+      },
+      reviews: {
+          type: Number
+      },
+      productImg: {
+        type: String
+      },
+      status: {
+        type: String,
+        default: 'ACTIVE',       
+      }
+})
+
+const Product = mongoose.model('products', productSchema)
+
+export default Product;
